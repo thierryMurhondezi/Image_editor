b0e6672b026e51ad3edbecf85875883d84412baf
Thierry Murhondezi Bisimwa <thierrymurhondezi@gmail.com>
2023-09-25 04:06:44 +0100

    add a new file index.html , a style.css , script.js

---
+++ script.js
@@ -0,0 +1,13 @@
+// Add functionality to buttons
+document.getElementById("crop-button").addEventListener("click", function() {
+    // Crop image logic
+  });
+  
+  document.getElementById("resize-button").addEventListener("click", function() {
+    // Resize image logic
+  });
+  
+  document.getElementById("filter-button").addEventListener("click", function() {
+    // Apply filter logic
+  });
+  
